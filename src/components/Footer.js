8194cf2a57474a991d9f84372736ba7d22c03aae
Ismael Morel <ismaelmorel84@gmail.com>
2021-11-24 01:55:06 -0300

    fixs components

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -31,8 +31,8 @@ const SocialButton = ({ children, label, href }) => {
       transition={'background 0.3s ease'}
       color="white"
       _hover={{
-        bg: useColorModeValue('blackAlpha.200', 'whiteAlpha.200'),
-        color: useColorModeValue('black', 'white'),
+        bg: 'whiteAlpha.200',
+        color: 'brand.100',
       }}
     >
       <VisuallyHidden>{label}</VisuallyHidden>
@@ -43,10 +43,7 @@ const SocialButton = ({ children, label, href }) => {
 
 export const Footer = () => {
   return (
-    <Box
-      bg={useColorModeValue('gray.50', 'gray.900')}
-      color={useColorModeValue('gray.700', 'gray.200')}
-    >
+    <Box bg="gray.50" color="gray.700">
       <Container
         as={Stack}
         maxW={'6xl'}
